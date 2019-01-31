251ff8d29e79bb371d3779b470d4513b09886ef0
TanakakanaT <46583551+TanakakanaT@users.noreply.github.com>
2019-01-31 19:11:39 +0900

    入力画面でokボタンを押すと次回起動時起動しないのでとりあえず力技で修正

--- SandL.java
+++ SandL.java
@@ -6,59 +6,60 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class SandL{
-	public String filename;
-	public String[][] data;
-	public File file;   
+    public String filename;
+    public String[][] data;
+    public File file;   
 
-	SandL(int koma,int youbi,String filename){
-		this.filename = filename;
-		data = new String[koma][youbi];
-		file = new File(filename);        
-	}
+    SandL(int koma,int youbi,String filename){
+	this.filename = filename;
+	data = new String[koma][youbi];
+	file = new File(filename);        
+    }
 
-	public void loadFile(){
-		try{
-			Scanner sc = new Scanner(file);
+    public void loadFile(){
+	try{
+	    Scanner sc = new Scanner(file);
 
-			for(int i=0;i<data.length;i++){
-				for(int j=0;j<data[i].length;j++){
-					data[i][j] = sc.nextLine();
-				}
-			}
-		}catch(IOException e){
-			System.err.print(e);
+	    for(int i=0;i<data.length;i++){
+		for(int j=0;j<data[i].length;j++){
+		    data[i][j] = sc.nextLine();
 		}
+	    }
+	}catch(IOException e){
+	    System.err.print(e);
 	}
+    }
 
-	public void saveFile(){
-		try{
-			FileWriter fw = new FileWriter(filename);
-			PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+    public void saveFile(){
+	try{
+	    FileWriter fw = new FileWriter(filename);
+	    PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
 
-			for(int i=0;i<5;i++){
-				for(int j=0;j<5;j++){
-					pw.println(data[i][j]);;
-				}
-			}
-			pw.close();
-		}catch(IOException e){
-			System.err.print(e);
+	    for(int i=0;i<6;i++){
+		for(int j=0;j<5;j++){
+		    pw.println(data[i][j]);;
 		}
+	    }
+	    pw.close();
+	}catch(IOException e){
+	    System.err.print(e);
 	}
+    }
 
-	public void save2File(){
-		try{
-			FileWriter fw = new FileWriter(filename);
-			PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
-
-			for(int j=0;j<5;j++){
-				pw.println(data[6][j]);;
-			}
-			pw.close();
-		}catch(IOException e){
-			System.err.print(e);
+    public void save2File(){
+	try{
+	    FileWriter fw = new FileWriter(filename);
+	    PrintWriter pw = new PrintWriter(new BufferedWriter(fw));
+	    for(int i=0;i<6;i++){
+		for(int j=0;j<5;j++){
+		    pw.println(data[i][j]);;
 		}
+	    }
+	    pw.close();
+	}catch(IOException e){
+	    System.err.print(e);
 	}
+    }
 }
 
     
